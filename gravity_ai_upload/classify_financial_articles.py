b17ec981b1c1489e0c1fdd934e9468b2f2f923ab
Andy Albrecht <aalbrecht33733@gmail.com>
2022-12-28 20:59:04 -0500

    Ania A.i. tool for Machine Learning model

---
+++ gravity_ai_upload/classify_financial_articles.py
@@ -0,0 +1,11 @@
+# Build & Sell A.I. Model using Gravity-ai.com
+# Machine Learning Assignment
+# Building an A.I. Tool to 
+# We must first train our model:
+# In Machine Learning, a set of data is called a 
+# 'corpus' = A corpus is a collection of authentic 
+# text or audio organized into datasets.
+# print("I am in LOVE with Ania Kubow! <3")
+from gravityai import gravityai as grav
+import pickle
+import pandas as pd
